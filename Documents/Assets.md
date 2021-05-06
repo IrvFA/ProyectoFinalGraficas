964dfec03a7d39db45e786d44f2ca30dcc8eef6f
Irving <irvingfa72@gmail.com>
2021-05-06 17:53:37 -0500

    Updated Assets, scene and tests

---
+++ Documents/Assets.md
@@ -0,0 +1,4 @@
+# List of assets with links for animation index
+
+## Wolf animations
+https://poly.google.com/view/dZYP3EIo65K
--- Story/story.js
+++ Story/story.js
@@ -2,6 +2,7 @@ import * as THREE from '../libs/three.js/r125/three.module.js'
 import { GLTFLoader } from '../libs/three.js/r125/loaders/GLTFLoader.js'
 import { OrbitControls } from '../libs/three.js/r125/controls/OrbitControls.js';
 import { OBJLoader } from '../libs/three.js/r125/loaders/OBJLoader.js';
+import { FBXLoader } from '../libs/three.js/r125/loaders/FBXLoader.js';
 import { MTLLoader } from '../libs/three.js/r125/loaders/MTLLoader.js';
 
 let renderer = null, scene = null, camera = null, root = null, group = null, orbitControls = null;
@@ -22,6 +23,11 @@ let rock2ModelUrl = {obj: "../Assets/Scene_1/rock2.obj", mtl: "../Assets/Scene_1
 let carModelUrl = {obj: "../Assets/Scene_1/car/toon_car.obj", mtl: "../Assets/Scene_1/car/toon_car.mtl"};
 let SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
 let floor = -2;
+let birdObjects = [];
+let modelUrls = ["../Assets/Scene_1/Flamingo.glb", "../Assets/Scene_1/Parrot.glb", "../Assets/Scene_1/Stork.glb"];
+let carObject = null;
+let pineTreeGroup = new THREE.Object3D, ballTreeGroup = new THREE.Object3D, rockGroup = new THREE.Object3D, carGroup = new THREE.Object3D;
+
 
 
 
@@ -33,6 +39,22 @@ function main()
     createScene(canvas);
     
     update();
+}
+
+function setVectorValue(vector, configuration, property, initialValues)
+{
+    if(configuration !== undefined)
+    {
+        if(property in configuration)
+        {
+            console.log("setting:", property, "with", configuration[property]);
+            vector.set(configuration[property].x, configuration[property].y, configuration[property].z);
+            return;
+        }
+    }
+
+    console.log("setting:", property, "with", initialValues);
+    vector.set(initialValues.x, initialValues.y, initialValues.z);
 }
 
 function degrees_to_radians(degrees)
@@ -52,44 +74,73 @@ function onProgress( xhr ) {
     }
 }
 
-// async function loadObj(modelUrls, objectList)
-// {
-//     try {
-//         const object = await new OBJLoader().loadAsync(modelUrls.obj, onProgress, onError);
-//         let texture = objModelUrl.hasOwnProperty('normalMap') ? new THREE.TextureLoader().load(objModelUrl.map) : null;
-//         let normalMap = objModelUrl.hasOwnProperty('normalMap') ? new THREE.TextureLoader().load(objModelUrl.normalMap) : null;
-//         let specularMap = objModelUrl.hasOwnProperty('specularMap') ? new THREE.TextureLoader().load(objModelUrl.specularMap) : null;
-
-//         console.log(object);
-        
-//         object.traverse(function (child) {
-//             if (child.isMesh) {
-//                 child.castShadow = true;
-//                 child.receiveShadow = true;
-//                 child.material.map = texture;
-//                 child.material.normalMap = normalMap;
-//                 child.material.specularMap = specularMap;
-//             }
-//         });
-
-//         object.scale.set(3, 3, 3);
-//         object.position.z = -6;
-//         object.position.x = -1.5;
-//         object.rotation.y = -3;
-//         object.name = "objObject";
-//         objectList.push(object);
-//         scene.add(object);
-
-//     }
-//     catch (err) {
-//         onError(err);
-//     }
-// }
-
-async function loadObjMtl(objModelUrl, objectList, position_x, position_z, scale)
+async function loadGLTF()
 {
+    const gltfLoader = new GLTFLoader();
+
+    const modelsPromises = modelUrls.map(url =>{
+        return gltfLoader.loadAsync(url);
+    });
+
     try
     {
+        const results = await Promise.all(modelsPromises);
+
+        results.forEach( (result, index) =>
+        {
+            console.log(result);
+
+            const object = result.scene.children[0];
+
+            object.scale.set( 0.02, 0.02, 0.02 );
+            object.rotation.y = 135;
+            object.position.x = index > 0 ?  - Math.random() * 10: 1;
+            object.position.y = index == 0 ?  6 :  4;
+            object.position.z = 20;
+
+            object.castShadow = true;
+            object. receiveShadow = true;
+
+            object.mixer = new THREE.AnimationMixer( scene );
+            object.action = object.mixer.clipAction( result.animations[0], object).setDuration( 1.0 );
+
+            object.action.play();
+            
+            birdObjects.push(object);           
+
+            scene.add(object);
+
+        });        
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
+}
+
+async function loadFBX(fbxModelUrl, configuration)
+{
+    try{
+        let object = await new FBXLoader().loadAsync(fbxModelUrl);
+
+        setVectorValue(object.position, configuration, 'position', new THREE.Vector3(0,0,0));
+        setVectorValue(object.scale, configuration, 'scale', new THREE.Vector3(1, 1, 1));
+        setVectorValue(object.rotation, configuration, 'rotation', new THREE.Vector3(0,0,0));
+        
+        scene.add( object );
+
+        return object;
+    }
+    catch(err)
+    {
+        console.error( err );
+    }
+}
+
+async function loadObjMtl(objModelUrl, objectList, position_x, position_z, scale, objectGroup)
+{
+    try
+    {   
         const mtlLoader = new MTLLoader();
 
         const materials = await mtlLoader.loadAsync(objModelUrl.mtl, onProgress, onError);
@@ -112,11 +163,13 @@ async function loadObjMtl(objModelUrl, objectList, position_x, position_z, scale
         object.position.x = position_x;
         object.position.y = floor;
         object.position.z = position_z;
-        
-        //object.scale.set(0.05, 0.05, 0.05);
+        object.rotation.y = 135;
 
         objectList.push(object);
-        group.add(object);
+        objectGroup.add(object)
+        group.add(objectGroup);
+
+
     }
     catch (err){
         onError(err);
@@ -161,7 +214,6 @@ function createScene(canvas)
     ambientLight = new THREE.AmbientLight ( 0x888888 );
     root.add(ambientLight);
     
-    //loadGLTF();
 
     group = new THREE.Object3D;
     root.add(group);
@@ -174,18 +226,18 @@ function createScene(canvas)
     let pineTreeScale = 0.5;
     let rockTreeScale = 3.0;
     for (let i=0; i<5; i++){
-        loadObjMtl(ballTreeModelUrl, objectList, 5, positionZ,ballTreeScale);
-        loadObjMtl(ballTreeModelUrl, objectList, -25, positionZ,ballTreeScale);
-        loadObjMtl(pineTreeModelUrl, objectList, -30, positionZ+5,pineTreeScale);
-        loadObjMtl(pineTreeModelUrl, objectList, 0, positionZ+5,pineTreeScale);
-        loadObjMtl(rock2ModelUrl, objectList, -10, positionZ+7,rockTreeScale);
-        loadObjMtl(rock2ModelUrl, objectList, 20, positionZ+7,rockTreeScale);
+        loadObjMtl(ballTreeModelUrl, objectList, 0, positionZ,ballTreeScale, ballTreeGroup);
+        loadObjMtl(ballTreeModelUrl, objectList, -27, positionZ,ballTreeScale, ballTreeGroup);
+        loadObjMtl(pineTreeModelUrl, objectList, -22, positionZ+5,pineTreeScale, pineTreeGroup);
+        loadObjMtl(pineTreeModelUrl, objectList, 5, positionZ+5,pineTreeScale, pineTreeGroup);
+        loadObjMtl(rock2ModelUrl, objectList, -15, positionZ+7,rockTreeScale, rockGroup);
+        loadObjMtl(rock2ModelUrl, objectList, -45, positionZ+7,rockTreeScale, rockGroup);
 
         positionZ -= 10;
     }
 
-    loadObjMtl(carModelUrl, objectList, -15.5, 0, 0.025);
-
+    loadObjMtl(carModelUrl, objectList, -15.5, 10, 0.025, carGroup);
+    loadGLTF();
     group.position.x += 10;
     scene.add(group);
     scene.add( root );
@@ -202,8 +254,23 @@ function update()
     orbitControls.update();
 }
 
-function animate(){
+async function animate() 
+{
+    const now = Date.now();
+    const deltat = now - currentTime;
+    currentTime = now;
 
+    for(const object of birdObjects)
+    {
+        object.position.z -= 0.01 * deltat;
+        
+        if(object.position.z < -50)
+            object.position.z = 20;
+
+        if(object.mixer)
+            object.mixer.update(deltat*0.001);
+    }
+    carGroup.position.z -=0.07;
 }
 
 
